9c65c644938a0aba94074a204e31ee3d2f92f0df
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-04-12 15:51:54 +0300

    Добавьте файлы проекта.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Book
+    {
+        public string Name { get; set; } = "book name";
+        public int Pages { get; set; }
+        public int Progress { get; set; }
+
+        public Book()
+        {
+            var random = new Random();
+            Pages = random.Next(100, 1001);
+            Progress = random.Next(101);
+        }
+    }
+}
---
+++ ViewModels/Books.cs
@@ -0,0 +1,34 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class Books
+    {
+        public ObservableCollection<Book> AppBooks { get; set; } = new ObservableCollection<Book>()
+        {
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book(),
+            new Book()
+        };
+    }
+}
